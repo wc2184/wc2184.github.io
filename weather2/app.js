66e6c16d648074258563e7966abf7abfa2320934
wc2184 <68214261+wc2184@users.noreply.github.com>
2021-12-26 21:31:55 -0500

    clear button

--- weather2/app.js
+++ weather2/app.js
@@ -5,13 +5,25 @@ const desc = document.querySelector(".desc");
 const icon = document.querySelector(".icon");
 const state = document.querySelector(".statename");
 const topimg = document.querySelector(".topimg");
+const card = document.querySelector(".card");
+const button = document.querySelector(".clear");
+const store = localStorage;
 let datafull;
+
+if (store.city != undefined) {
+  console.log(store.city, "stored", store.city);
+  console.log(store.city);
+  getWeather(store.city)
+    .then((data) => updateUI(data))
+    .catch((err) => errorUI());
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const location = input.value.trim();
-  console.log(location);
+  console.log(location, "real location: ");
   input.value = "";
+  store.setItem("city", location);
 
   // get the objects with an async function, then use .then to pass the output
   // into ANOTHER non async function that updates the UI. So do it all in one .then/.catch statement
@@ -27,7 +39,6 @@ form.addEventListener("submit", (e) => {
 // };
 
 const updateUI = (data) => {
-  console.log(data, "hi");
   console.log(data.location.country);
   console.log(data.current.temp_f, "degrees");
   cityname.textContent = data.location.name;
@@ -41,11 +52,29 @@ const updateUI = (data) => {
     "src",
     data.current.is_day ? "./img/day.svg" : "./img/night.svg"
   );
+  card.classList.remove("d-none");
+  button.classList.remove("d-none");
+  button.classList.add("d-block");
 };
 
 const errorUI = () => {
+  store.removeItem("city");
+  card.classList.remove("d-none");
+  button.classList.add("d-block");
+
   cityname.textContent = "City not found.";
   state.textContent = "Please try again.";
   icon.setAttribute("src", "");
   desc.textContent = "";
 };
+
+const clear = () => {
+  localStorage.clear();
+  card.classList.add("d-none");
+  button.classList.remove("d-block");
+  button.classList.add("d-none");
+};
+button.addEventListener("click", () => {
+  console.log("pressed");
+  clear();
+});
